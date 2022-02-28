30b2915fdec26aba6350a423de7bafe9ff4a2f97
Christina Sugiono <christina.sugiono95@gmail.com>
2022-02-28 17:35:16 +0800

    added has_many reviews

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
-  has_many :users, through: :bookings
+  has_many :reviews, through: :bookings
 
   validates :name, presence: true, uniqueness: true
   validates :description, :price, :brand, :category, presence: true
-
 end
