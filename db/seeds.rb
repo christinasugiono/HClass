9e775f7c636c53d6ae7230d493b2f026792621c1
jessica wong <jsica7@gmail.com>
2022-02-28 16:57:42 +0800

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# user - 3
+# items - 10
+
+require 'faker'
+
+5.times do
+  user = User.new(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    address: Faker::Address.street_name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+  )
+  user.save!
+end
+
+10.times do
+  item = Item.new(
+    name: Faker::Commerce.product_name,
+    description: Faker::Lorem.sentences(number: 1),
+    price: Faker::Commerce.price(range: 50..500),
+    brand: Faker::Commerce.brand,
+    category: Faker::Commerce.department,
+    user_id: rand(1..5)
+  )
+  item.save!
+end
+puts 'Finished!'
