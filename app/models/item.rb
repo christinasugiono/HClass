41a544eabb06f44f030fa35b15b40c12c243375c
Christina Sugiono <christina.sugiono95@gmail.com>
2022-03-01 12:50:01 +0800

    added categories for item

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,9 @@ class Item < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :reviews, through: :bookings
   has_one_attached :photo
+  CATEGORIES = ["Clothes", "Shoes", "Bags", "Accesories"]
 
   validates :name, presence: true, uniqueness: true
-  validates :description, :price, :brand, :category, presence: true
+  validates :description, :price, :brand, presence: true
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
 end
