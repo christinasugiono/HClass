b2c63e0f0852b5aa6060b171a96f5c8b42b002e8
Christina Sugiono <christina.sugiono95@gmail.com>
2022-03-02 11:04:55 +0800

    added API key

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,9 @@
 class Item < ApplicationRecord
+  CATEGORIES = ["Clothes", "Shoes", "Bags", "Accesories"]
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_many :reviews, through: :bookings
   has_one_attached :photo
-  CATEGORIES = ["Clothes", "Shoes", "Bags", "Accesories"]
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
