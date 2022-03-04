b1414e5c3f52307588e506e227b31eff1b9bd8c7
jessica wong <jsica7@gmail.com>
2022-03-04 18:02:09 +0800

    show

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,23 +7,3 @@ import { definitionsFromContext } from "stimulus/webpack-helpers"
 const application = Application.start()
 const context = require.context("controllers", true, /_controller\.js$/)
 application.load(definitionsFromContext(context))
-console.log('hello')
-
-// Target the button
-const btn = document.getElementById("favourite");
-// Target the text
-const txt = document.querySelector(".text");
-// Add an evnet listener on the button (CLICK event)
-
-if (btn) {
-  document.addEventListener("turbolinks:load", () => {
-    btn.addEventListener("click", (event) => {
-      const icon = btn.querySelector("i");
-      icon.classList.remove("far");
-      icon.classList.add("fas");
-      txt.classList.remove("d-none");
-      console.log(txt.innerHTML);
-    });
-    // Remove the class d-none from text
-  })
-}
--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -47,6 +47,8 @@ export default class extends Controller {
     const bounds = new mapboxgl.LngLatBounds()
     console.log(this.markersValue)
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 2000 })
+    if (this.markersValue.length !== 0) {
+      this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 2000 })
+    }
   }
 }
